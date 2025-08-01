cffa7241d1b863bb52f7de1fd5f1a01c5e2729b9
The Deanominator <thedean.colak@outlook.com>
2025-08-01 04:02:17 -0400

    Delete MacUtilGUI directory
    Removing GUI as only interested in .sh scripts.

--- MacUtilGUI/Models/ScriptCategory.fs
+++
@@ -1,5 +0,0 @@
-namespace MacUtilGUI.Models
-
-type ScriptCategory =
-    { Name: string
-      Scripts: ScriptInfo list }
--- MacUtilGUI/Models/ScriptInfo.fs
+++
@@ -1,9 +0,0 @@
-namespace MacUtilGUI.Models
-
-type ScriptInfo =
-    { Name: string
-      Description: string
-      Script: string
-      TaskList: string
-      Category: string
-      FullPath: string }
