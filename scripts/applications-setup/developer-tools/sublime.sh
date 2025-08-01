533155c3aab64ef9aa0d91f4587af0b7288a0951
The Deanominator <thedean.colak@outlook.com>
2025-08-01 04:19:31 -0400

    Update sublime.sh
    Replace Sublime with Pulsar

--- scripts/applications-setup/developer-tools/sublime.sh
+++ scripts/applications-setup/developer-tools/sublime.sh
@@ -2,15 +2,15 @@
 
 . ../../common-script.sh
 
-installSublime() {
-    if ! brewprogram_exists sublime-text; then
-        printf "%b\n" "${YELLOW}Installing Sublime...${RC}"
-        brew install --cask sublime-text
+installPulsar() {
+    if ! brewprogram_exists pulsar; then
+        printf "%b\n" "${YELLOW}Installing Pulsar...${RC}"
+        brew install --cask pulsar
         if [ $? -ne 0 ]; then
-            printf "%b\n" "${RED}Failed to install Sublime. Please check your Homebrew installation or try again later.${RC}"
+            printf "%b\n" "${RED}Failed to install Pulsar. Please check your Homebrew installation or try again later.${RC}"
             exit 1
         fi
-        printf "%b\n" "${GREEN}Sublime installed successfully!${RC}"
+        printf "%b\n" "${GREEN}Pulsar installed successfully!${RC}"
     else
         printf "%b\n" "${GREEN}Sublime is already installed.${RC}"
     fi
